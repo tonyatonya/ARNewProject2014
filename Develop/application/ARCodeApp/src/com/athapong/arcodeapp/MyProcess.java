2a260dabc3271aff3c4258b0a5f6c3a1dd231206
Athapong Athamasirigun <tonya_0@hotmail.com>
2014-04-12 18:02:57 +0700

    Create Service

--- Develop/application/ARCodeApp/src/com/athapong/arcodeapp/MyProcess.java
+++ Develop/application/ARCodeApp/src/com/athapong/arcodeapp/MyProcess.java
@@ -2,4 +2,34 @@ package com.athapong.arcodeapp;
 
 public class MyProcess {
 
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return super.equals(o);
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
 }
