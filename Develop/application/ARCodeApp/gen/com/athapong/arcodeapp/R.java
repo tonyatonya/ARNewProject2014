2760ebfa90846f248ae780fdd80c4c12b40a56f3
Athapong Athamasirigun <tonya_0@hotmail.com>
2014-04-15 18:12:58 +0700

    add openCV Lib

--- Develop/application/ARCodeApp/gen/com/athapong/arcodeapp/R.java
+++ Develop/application/ARCodeApp/gen/com/athapong/arcodeapp/R.java
@@ -9,9 +9,35 @@ package com.athapong.arcodeapp;
 
 public final class R {
     public static final class attr {
+        /** <p>May be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+<p>May be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>any</code></td><td>-1</td><td></td></tr>
+<tr><td><code>back</code></td><td>99</td><td></td></tr>
+<tr><td><code>front</code></td><td>98</td><td></td></tr>
+</table>
+         */
+        public static int camera_id=0x7f010001;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static int show_fps=0x7f010000;
     }
     public static final class color {
-        public static final int background=0x7f040000;
+        public static int background=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -20,67 +46,70 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static int activity_horizontal_margin=0x7f060000;
+        public static int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int SignIn=0x7f090007;
-        public static final int SignUp=0x7f090008;
-        public static final int action_settings=0x7f09000e;
-        public static final int bizList=0x7f090009;
-        public static final int button1=0x7f090001;
-        public static final int button2=0x7f090003;
-        public static final int capture=0x7f09000a;
-        public static final int editText1=0x7f090005;
-        public static final int editText2=0x7f090006;
-        public static final int editprofile=0x7f09000b;
-        public static final int imageView1=0x7f090000;
-        public static final int logout=0x7f09000d;
-        public static final int mylist=0x7f09000c;
-        public static final int targeturi=0x7f090002;
-        public static final int textView1=0x7f090004;
+        public static int SignIn=0x7f04000a;
+        public static int SignUp=0x7f04000b;
+        public static int action_settings=0x7f040011;
+        public static int any=0x7f040000;
+        public static int back=0x7f040001;
+        public static int bizList=0x7f04000c;
+        public static int button1=0x7f040004;
+        public static int button2=0x7f040006;
+        public static int capture=0x7f04000d;
+        public static int editText1=0x7f040008;
+        public static int editText2=0x7f040009;
+        public static int editprofile=0x7f04000e;
+        public static int front=0x7f040002;
+        public static int imageView1=0x7f040003;
+        public static int logout=0x7f040010;
+        public static int mylist=0x7f04000f;
+        public static int targeturi=0x7f040005;
+        public static int textView1=0x7f040007;
     }
     public static final class layout {
-        public static final int activity_capture=0x7f030000;
-        public static final int activity_editprofile=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_mylist=0x7f030003;
-        public static final int activity_myprocess=0x7f030004;
-        public static final int activity_register=0x7f030005;
-        public static final int activity_welcome=0x7f030006;
+        public static int activity_capture=0x7f030000;
+        public static int activity_editprofile=0x7f030001;
+        public static int activity_main=0x7f030002;
+        public static int activity_mylist=0x7f030003;
+        public static int activity_myprocess=0x7f030004;
+        public static int activity_register=0x7f030005;
+        public static int activity_welcome=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int btnName_Cancle=0x7f060016;
-        public static final int btnName_Capture=0x7f06000f;
-        public static final int btnName_Logout=0x7f060011;
-        public static final int btnName_MyBizList=0x7f060010;
-        public static final int btnName_Register=0x7f060014;
-        public static final int btnName_Save=0x7f060015;
-        public static final int btnName_TakePhoto=0x7f060017;
-        public static final int btnName_editProfile=0x7f06000e;
-        public static final int btnName_signUp=0x7f060013;
-        public static final int btnName_signin=0x7f060012;
-        public static final int inputName_Address=0x7f06000d;
-        public static final int inputName_Email=0x7f06000c;
-        public static final int inputName_Name=0x7f06000b;
-        public static final int inputName_Password=0x7f06000a;
-        public static final int inputName_Username=0x7f060009;
-        public static final int pageTitle_BizInfo=0x7f060007;
-        public static final int pageTitle_Capture=0x7f060004;
-        public static final int pageTitle_EditProfile=0x7f060005;
-        public static final int pageTitle_Login=0x7f060002;
-        public static final int pageTitle_MyProcess=0x7f060008;
-        public static final int pageTitle_Register=0x7f060006;
-        public static final int pageTitle_Welcome=0x7f060003;
-        public static final int tap=0x7f060018;
+        public static int action_settings=0x7f070001;
+        public static int app_name=0x7f070000;
+        public static int btnName_Cancle=0x7f070016;
+        public static int btnName_Capture=0x7f07000f;
+        public static int btnName_Logout=0x7f070011;
+        public static int btnName_MyBizList=0x7f070010;
+        public static int btnName_Register=0x7f070014;
+        public static int btnName_Save=0x7f070015;
+        public static int btnName_TakePhoto=0x7f070017;
+        public static int btnName_editProfile=0x7f07000e;
+        public static int btnName_signUp=0x7f070013;
+        public static int btnName_signin=0x7f070012;
+        public static int inputName_Address=0x7f07000d;
+        public static int inputName_Email=0x7f07000c;
+        public static int inputName_Name=0x7f07000b;
+        public static int inputName_Password=0x7f07000a;
+        public static int inputName_Username=0x7f070009;
+        public static int pageTitle_BizInfo=0x7f070007;
+        public static int pageTitle_Capture=0x7f070004;
+        public static int pageTitle_EditProfile=0x7f070005;
+        public static int pageTitle_Login=0x7f070002;
+        public static int pageTitle_MyProcess=0x7f070008;
+        public static int pageTitle_Register=0x7f070006;
+        public static int pageTitle_Welcome=0x7f070003;
+        public static int tap=0x7f070018;
     }
     public static final class style {
         /** 
@@ -104,10 +133,65 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static int AppTheme=0x7f080001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CameraBridgeViewBase.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CameraBridgeViewBase_camera_id com.athapong.arcodeapp:camera_id}</code></td><td></td></tr>
+           <tr><td><code>{@link #CameraBridgeViewBase_show_fps com.athapong.arcodeapp:show_fps}</code></td><td></td></tr>
+           </table>
+           @see #CameraBridgeViewBase_camera_id
+           @see #CameraBridgeViewBase_show_fps
+         */
+        public static final int[] CameraBridgeViewBase = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.athapong.arcodeapp.R.attr#camera_id}
+          attribute's value can be found in the {@link #CameraBridgeViewBase} array.
+
+
+          <p>May be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+<p>May be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>any</code></td><td>-1</td><td></td></tr>
+<tr><td><code>back</code></td><td>99</td><td></td></tr>
+<tr><td><code>front</code></td><td>98</td><td></td></tr>
+</table>
+          @attr name com.athapong.arcodeapp:camera_id
+        */
+        public static final int CameraBridgeViewBase_camera_id = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.athapong.arcodeapp.R.attr#show_fps}
+          attribute's value can be found in the {@link #CameraBridgeViewBase} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.athapong.arcodeapp:show_fps
+        */
+        public static final int CameraBridgeViewBase_show_fps = 0;
+    };
 }
--- Develop/application/ARCodeApp/src/com/athapong/arcodeapp/MyProcess.java
+++ Develop/application/ARCodeApp/src/com/athapong/arcodeapp/MyProcess.java
@@ -1,8 +1,10 @@
 package com.athapong.arcodeapp;
 
+
 import android.app.Activity;  
 import android.content.Intent;  
-import android.os.Bundle; 
+import android.os.Bundle;
+
 
 public class MyProcess extends Activity {
 
